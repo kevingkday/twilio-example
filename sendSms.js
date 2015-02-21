5789d0324ca791ada6f79507af666e4afa231d38
riskebiz <kevin@riskebiz.com>
2015-02-20 17:10:38 -0800

    Update sendSms.js

--- sendSms.js
+++ sendSms.js
@@ -1,7 +1,7 @@
-var twilio = require("../twilio-node/lib");
+var twilio = require('../twilio-node/lib');
 // Your accountSid and authToken from twilio.com/user/account
 var accountSid = 'AC140d1e9dd765e6d26b90dd90f7b38cf0';
-var authToken = "7d2b0abc46997462364f8b2de6ecd001";
+var authToken = '7d2b0abc46997462364f8b2de6ecd001';
 var client = require(twilio)(accountSid, authToken);
 var fs = require('fs');
 var http = require('http');
