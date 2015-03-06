aecd2082f36740a420ec65fd1648693b85d60710
riskebiz <kevin@riskebiz.com>
2015-03-05 16:22:20 -0800

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,20 +1,49 @@
-// Your accountSid and authToken from twilio.com/user/account
-var accountSid = 'AC140d1e9dd765e6d26b90dd90f7b38cf0';
-var authToken = '7d2b0abc46997462364f8b2de6ecd001';
-var client = require('twilio')(accountSid, authToken);
-var fs = require('fs');
-var http = require('http');
+//document.write("Hello, Rodrigo");
 
-var server = http.createServer(function (request, response) {
-    var indexHtml = fs.readFileSync('public_html/default.html');
-    response.client.messages.create({
-        body: "Jenny please?! I love you <3",
-        to: "+16048385077",
-        from: "+19177467475"}, function (err, message) {process.stdout.write(message.sid);
-      
-        response.end(indexHtml);
-        })
 
-});
+//var xhr = new XMLHttpRequest();
+//xhr.open("GET", "https://api.ripple.com/v1/wallet/new", false);
+//xhr.send();
 
-server.listen(8000);
+//document.write(xhr.response);
+//document.write(xhr.responseText);
+
+
+
+//var demo = (xhr.responseText);
+
+//var json = JSON.parse(demo);
+
+//var output = json.wallet.address;
+//document.write(output);
+
+
+var xhs = new XMLHttpRequest();
+xhs.open("GET", "https://api.ripple.com/v1/accounts/rDxMDiegComKQLiPoQ4hzS2kpUu4UxTWon/balances?currency=BTC", false);
+xhs.send();
+
+//document.write(xhr.response);
+//document.write(xhr.responseText);
+
+//document.write(xhs.responseText);
+
+var balance = (xhs.responseText);
+//document.write(balance);
+var total = JSON.parse(balance);
+
+var totaloutput = total.balances[0].value;
+
+var xmlhttp;
+
+xmlhttp = new XMLHttpRequest();
+
+//xmlhttp.onreadystatechange=function()
+//{
+xmlhttp.open("POST", "http://localhost:57399/mobilikey/service/Sms.aspx", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("From=%2B16048385077&To=%2B16042103679&Body="+totaloutput);
+    document.write(totaloutput);
+//}
+
+
+    server.listen(8000);
