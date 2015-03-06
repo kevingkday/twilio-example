99abf92bab5067d4316c019ca1572310e87f13b5
riskebiz <kevin@riskebiz.com>
2015-03-06 11:37:48 -0800

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var fs = require('fs');
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
 var app = express();
 
 app.get('/', function(req, res) {
  res.status(200)
 // .send({ success: true })
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+ var xhr = new XMLHttpRequest();
+ xhr.open("GET", "https://api.ripple.com/v1/accounts/rDxMDiegComKQLiPoQ4hzS2kpUu4UxTWon/balances?currency=BTC", false);
+ xhr.send();
  
- var xhs = new XMLHttpRequest();
- xhs.open("GET", "https://api.ripple.com/v1/accounts/rDxMDiegComKQLiPoQ4hzS2kpUu4UxTWon/balances?currency=BTC", false);
-// xhs.send();
- 
- .send({ success: true })
+// .send({ success: true })
 
 });
 
