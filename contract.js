2c6a1fcfd7432240d9f058398557d896ffd17c85
riskebiz <kevin@riskebiz.com>
2015-03-06 15:18:01 -0800

    Update contract.js

--- contract.js
+++ contract.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res) {
     xhr.open("GET", "https://api.ripple.com/v1/accounts/rDxMDiegComKQLiPoQ4hzS2kpUu4UxTWon/balances?currency=BTC", false);
     xhr.send();
 
-    var balance = (xhr.responseText);
+    var balance = "blahblah";
 
  
 
