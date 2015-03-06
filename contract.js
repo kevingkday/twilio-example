9759555678a5a7e5f18d5e0a60c588c536df31a2
riskebiz <kevin@riskebiz.com>
2015-03-06 11:00:07 -0800

    Update contract.js

--- contract.js
+++ contract.js
@@ -3,25 +3,10 @@ var fs = require('fs');
 var app = express();
 
 app.get('/', function(req, res) {
+
 res.status(200)
+.send({ success: true })
 
-var xhs = new XMLHttpRequest();
-xhs.open("GET", "https://api.ripple.com/v1/accounts/rDxMDiegComKQLiPoQ4hzS2kpUu4UxTWon/balances?currency=BTC", false);
-xhs.send();
-
-var balance = (xhs.responseText);
-
-var total = JSON.parse(balance);
-
-var totaloutput = total.balances[0].value;
-
-var xmlhttp;
-
-xmlhttp = new XMLHttpRequest();
-
-xmlhttp.open("POST", "http://www.mobilikey.com/service/Sms.aspx", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send("From=%2B16048385077&To=%2B16042103679&Body="+totaloutput);
 });
 
 app.listen(process.env.PORT || 5000);
