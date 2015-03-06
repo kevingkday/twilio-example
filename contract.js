9dcc43e51273e0741f4f55795329cceaa0b9fe99
riskebiz <kevin@riskebiz.com>
2015-03-06 11:11:06 -0800

    Update contract.js

--- contract.js
+++ contract.js
@@ -3,10 +3,8 @@ var fs = require('fs');
 var app = express();
 
 app.get('/', function(req, res) {
-
-res.status(200)
-.send({ success: true })
-
+  res.set('Content-Type', 'text/plain');
+  res.send(fs.readFileSync('contract.js', 'utf-8')).end();
 });
 
 app.listen(process.env.PORT || 5000);
