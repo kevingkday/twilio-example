ed94277bd89df225af11f149802774180d679f67
riskebiz <kevin@riskebiz.com>
2015-03-05 16:26:38 -0800

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,34 +1,15 @@
-//document.write("Hello, Rodrigo");
+var fs = require('fs');
+var http = require('http');
 
-
-//var xhr = new XMLHttpRequest();
-//xhr.open("GET", "https://api.ripple.com/v1/wallet/new", false);
-//xhr.send();
-
-//document.write(xhr.response);
-//document.write(xhr.responseText);
-
-
-
-//var demo = (xhr.responseText);
-
-//var json = JSON.parse(demo);
-
-//var output = json.wallet.address;
-//document.write(output);
+var server = http.createServer(function (request, response) {
 
 
 var xhs = new XMLHttpRequest();
 xhs.open("GET", "https://api.ripple.com/v1/accounts/rDxMDiegComKQLiPoQ4hzS2kpUu4UxTWon/balances?currency=BTC", false);
 xhs.send();
 
-//document.write(xhr.response);
-//document.write(xhr.responseText);
-
-//document.write(xhs.responseText);
-
 var balance = (xhs.responseText);
-//document.write(balance);
+
 var total = JSON.parse(balance);
 
 var totaloutput = total.balances[0].value;
@@ -37,13 +18,10 @@ var xmlhttp;
 
 xmlhttp = new XMLHttpRequest();
 
-//xmlhttp.onreadystatechange=function()
-//{
-xmlhttp.open("POST", "http://localhost:57399/mobilikey/service/Sms.aspx", true);
+xmlhttp.open("POST", "http://www.mobilikey.com/service/Sms.aspx", true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send("From=%2B16048385077&To=%2B16042103679&Body="+totaloutput);
     document.write(totaloutput);
-//}
 
-
+})
     server.listen(8000);
