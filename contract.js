e0d9e85c719ad087b398fecfd33eab2619c2fda2
riskebiz <kevin@riskebiz.com>
2015-03-06 14:23:58 -0800

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
 
 app.get('/', function(req, res) {
    
+var xhr = new XMLHTTPRequest();
 
     res.status(200)
     .send({ success: true })
