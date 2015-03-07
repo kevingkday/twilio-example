9183bca1236c4641308e4993d9a6b5a2dcb7b9b2
riskebiz <kevin@riskebiz.com>
2015-03-06 16:24:53 -0800

    Update contract.js

--- contract.js
+++ contract.js
@@ -8,12 +8,12 @@ app.get('/', function(req, res) {
    
     var xhr = new XMLHttpRequest();
     
-   xhr.open('GET', 'http://www.w3schools.com/ajax/demo_get.asp', false);
-   //xhr.open('GET', 'https://api.ripple.com/v1/accounts/rDxMDiegComKQLiPoQ4hzS2kpUu4UxTWon/balances?currency=BTC');
+   
+   xhr.open('GET', 'https://api.ripple.com/v1/accounts/rDxMDiegComKQLiPoQ4hzS2kpUu4UxTWon/balances?currency=BTC', false);
     xhr.send();
 
-  // var balance = (xhr.responseText);
-   var balance = '{ "success": true, "ledger": 12106156, "validated": true, "balances": [ { "value": "0.00718421768538286", "currency": "BTC", "counterparty": "rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q" } ] }'
+   var balance = (xhr.responseText);
+ //  var balance = '{ "success": true, "ledger": 12106156, "validated": true, "balances": [ { "value": "0.00718421768538286", "currency": "BTC", "counterparty": "rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q" } ] }'
    
   // var total = JSON.parse(balance);
 
