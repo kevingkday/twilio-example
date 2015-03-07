fc8bc9d6fddb3759a7a9b105d220630b6fcdc131
riskebiz <kevin@riskebiz.com>
2015-03-06 16:30:53 -0800

    Update contract.js

--- contract.js
+++ contract.js
@@ -6,13 +6,13 @@ var XMLHttpRequest = require('w3c-xmlhttprequest').XMLHttpRequest;
 
 app.get('/', function(req, res) {
    
-    var xhr = new XMLHttpRequest();
+  //  var xhr = new XMLHttpRequest();
     
    
-   xhr.open('GET', 'https://api.ripple.com/v1/accounts/rDxMDiegComKQLiPoQ4hzS2kpUu4UxTWon/balances?currency=BTC', false);
-    xhr.send();
+ //  xhr.open('GET', 'https://api.ripple.com/v1/accounts/rDxMDiegComKQLiPoQ4hzS2kpUu4UxTWon/balances?currency=BTC', false);
+//    xhr.send();
 
-   var balance = (xhr.responseText);
+  // var balance = (xhr.responseText);
  //  var balance = '{ "success": true, "ledger": 12106156, "validated": true, "balances": [ { "value": "0.00718421768538286", "currency": "BTC", "counterparty": "rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q" } ] }'
    
   // var total = JSON.parse(balance);
@@ -28,7 +28,7 @@ xmlhttp = new XMLHttpRequest();
     res.status(200)
 
     
- .send({ success: true, balance: balance })
+ .send({ success: true })
 
 });
 
